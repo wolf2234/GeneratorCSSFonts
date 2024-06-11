afc4ba40d30c5f80105ef64df76f38463a265ccb
wolf2234 <viktoral9599@gmail.com>
2024-06-11 12:33:21 +0300

    this commit adds local('').

--- generator_css-fonts.py
+++ generator_css-fonts.py
@@ -1,21 +1,22 @@
 import os
 file_names = []
 
-fl = os.listdir('')
+fl = os.listdir('C:/Users/avtom/Visual_Studio_Code/Projects/Test_projects/FreelanceOfLife/cesis/fonts/OpenSans')
 
 for i in fl:
     file_names.append(i.split('.')[0])
 
 for j in set(file_names):
-    path = "../fonts/NunitoSans"
+    path = "../fonts/OpenSans"
     str = "@font-face {" \
           "\n" \
-          "    font-family: 'NunitoSans'; \n" \
-          f"    src: url('{path}/{j}.woff2') format('woff2'), \n" \
-          f"         url('{path}/{j}.woff') format('woff'); \n" \
-          "    font-weight: normal; \n" \
-          "    font-style: normal; \n" \
-          "    font-display: swap; \n" \
+          "    font-family: 'OpenSans';\n" \
+          f"    src: local('{j}'),\n" \
+          f"         url('{path}/{j}.woff2') format('woff2'),\n" \
+          f"         url('{path}/{j}.woff') format('woff');\n" \
+          "    font-weight: normal;\n" \
+          "    font-style: normal;\n" \
+          "    font-display: swap;\n" \
           "}"
     print(str)
     print('\n')
